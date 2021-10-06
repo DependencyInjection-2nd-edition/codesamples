e8a5f7bdcb27c4ee7af59a28fabf78b8dee5a4c0
dotnetjunkie <steven@cuttingedge.it>
2021-10-06 21:57:20 +0200

    Added the ASP.NET Core Middleware examples from section 7.3.2 to the AopECommerce/PureDI example project.

---
+++ AopECommerce/src/Commerce.Web.PureDI/LoggingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Ploeh.Samples.Commerce.Web.PureDI
+{
+    // ---- Start code snippet section 7.3.2, page 233 ----
+    public class LoggingMiddleware
+    {
+        private readonly ILogger logger;
+
+        public LoggingMiddleware(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context, Func<Task> next)
+        {
+            logger.LogInformation("Request started");
+            await next();
+            logger.LogInformation("Request ended");
+        }
+    }
+    // ---- End code snippet section 7.3.2, page 233 ----
+}
